c538bdd8b429e4156571645c5d950476825c1307
Evans Odero <evansowinodero@gmail.com>
2025-11-14 06:34:21 +0300

    stream line locations

--- resources/views/admin/locations.blade.php
+++ resources/views/admin/locations.blade.php
@@ -20,11 +20,11 @@
                 <table class="table-fixed min-w-full divide-y divide-gray-200" id="locations_table">
                     <thead class="bg-gray-100">
                         <tr>
-                            <th class="p-4">#</th>
-                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">County</th>
-                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Sub County</th>
-                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Ward</th>
-                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Town</th>
+                            <th class="p-4 w-12">#</th>
+                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Name</th>
+                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Code</th>
+                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Parent</th>
+                            <th class="p-4 text-left text-xs font-medium text-gray-500 uppercase">Level</th>
                             <th class="p-4">
                                 <button id="open-modal-btn" class="text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium inline-flex items-center justify-center rounded-lg text-sm px-3 py-2">
                                     <svg class="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
@@ -85,11 +85,11 @@ $(document).ready(function () {
         ordering: false,
         ajax: "{{ route('admin.locations.index') }}",
         columns: [
-            { data: 'DT_RowIndex', name: 'DT_RowIndex' },
-            { data: 'county', name: 'county' },
-            { data: 'sub_county', name: 'sub_county' },
-            { data: 'ward', name: 'ward' },
-            { data: 'town', name: 'town' },
+            { data: 'DT_RowIndex', name: 'DT_RowIndex', searchable: false },
+            { data: 'name', name: 'name' },
+            { data: 'code', name: 'code' },
+            { data: 'parent_name', name: 'parent.name' },
+            { data: 'level_name', name: 'level_name' },
             { data: 'action', name: 'action', orderable: false, searchable: false },
         ]
     });
@@ -114,20 +114,37 @@ $(document).ready(function () {
                     timer: 3000
                 });
                 if (response.status === "success") {
+
                     $("#locationForm")[0].reset();
                     modal.hide();
                     table.ajax.reload(null, false);
                 }
             },
-            error: function () {
-                Swal.fire({
-                    toast: true,
-                    position: 'top-end',
-                    icon: 'error',
-                    title: 'Upload failed!',
-                    showConfirmButton: false,
-                    timer: 3000
-                });
+            error: function (xhr) {
+                let res = xhr.responseJSON;
+                if (res && res.errors) {
+                    let messages = Object.values(res.errors).flat().join("\n");
+                    Swal.fire({
+                        toast: true,
+                        position: 'top-end',
+                        icon: 'error',
+                        title: "Validation failed:\n" + messages,
+                        showConfirmButton: false,
+                        timer: 9000,
+                        timerProgressBar: true
+                    });
+                }
+                else {
+                    Swal.fire({
+                        toast: true,
+                        position: 'top-end',
+                        icon: 'error',
+                        title: 'Something went wrong',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true
+                    });
+                }
             }
         });
     });
