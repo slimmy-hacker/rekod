7ec7126122889dade57edba6ea2631b6490d74b5
slimmy_hacker <jamesgikwa68@gmail.com>
2026-06-17 19:12:49 +0100

    Fix register route method name mismatch (corrected)

--- routes/auth.php
+++ routes/auth.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
@@ -12,9 +12,8 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('/register', [RegisteredUserController::class, 'showRegisterForm'])
-    ->name('register');
-
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
     Route::post('/register', [RegisteredUserController::class, 'store'])
         ->name('register.store');
@@ -22,7 +21,7 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']) ->name('login.store');;
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 
     Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
