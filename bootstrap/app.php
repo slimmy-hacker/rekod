dffff5a1db86f3404399cac7bd1a4b70e9fd2248
slimmy_hacker <jamesgikwa68@gmail.com>
2026-06-24 22:50:16 +0100

    Fix bootstrap app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+
         then: function () {
+
             Route::prefix('students')
                 ->middleware(['web', 'portal:student'])
                 ->group(base_path('routes/student.php'));
@@ -32,12 +34,18 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->group(base_path('routes/company.php'));
         }
     )
+
     ->withMiddleware(function (Middleware $middleware): void {
+
         $middleware->alias([
             'portal' => \App\Http\Middleware\PortalMiddleWare::class,
             'ensure.attachment.selected' => \App\Http\Middleware\EnsureAttachmentSelected::class,
         ]);
+
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+
+    ->create();
